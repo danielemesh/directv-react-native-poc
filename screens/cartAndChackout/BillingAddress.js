e25b467a4161c02451439f3c0ad3154ab1de38e0
daniel <danielemesh@gmail.com>
2018-07-04 15:03:08 +0300

    Billing address BL

--- screens/cartAndChackout/BillingAddress.js
+++ screens/cartAndChackout/BillingAddress.js
@@ -1,41 +1,67 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import theme from '../../theme';
-import LabeledTextInput from '../../components/common/LabeledTextInput';
-import LabeledIcon from '../../components/common/LabeledIcon';
+import { StyleSheet } from 'react-native';
 import { Button, Text } from 'react-native-elements';
-import LabeledPicker from '../../components/common/LabeledPicker';
+
+import { LabeledTextInput, LabeledIcon, LabeledPicker, Panel, PanelHeader } from '../../components';
+import theme from '../../theme';
 
 class BillingAddress extends React.Component {
   constructor(props) {
     super(props);
     
-    this.state = {
-      selectedState: ''
+    this.controls = {
+      attention: React.createRef(),
+      addressLine1: React.createRef(),
+      addressLine2: React.createRef(),
+      city: React.createRef(),
+      state: React.createRef(),
+      zipCode: React.createRef()
     };
     
-    this.onChangeText    = this.onChangeText.bind(this);
-    this.onStateSelected = this.onStateSelected.bind(this);
-  }
-  
-  onChangeText(text) {
-    console.log(text);
-  }
-  
-  onStateSelected(itemValue, itemIndex) {
-    console.log(itemValue);
-    this.setState({selectedState: itemValue});
+    this.state = {
+      attention: '',
+      addressLine1: '',
+      addressLine2: '',
+      city: '',
+      state: '',
+      zipCode: ''
+    };
     
+    //this.state = {
+    //  selectedState: ''
+    //};
+    
+    this.onChangeText    = this.onChangeText.bind(this);
+    this.focusNextControl = this.focusNextControl.bind(this);
+    //this.onStateSelected = this.onStateSelected.bind(this);
   }
+  
+  focusNextControl(key) {
+    this.controls[key].focus();
+  }
+  
+  onChangeText(text, key) {
+    this.setState({
+      [key]: text
+    })
+  }
+  
+  //onStateSelected(itemValue, itemIndex) {
+  //  console.log(itemValue);
+  //  this.setState({selectedState: itemValue});
+  //
+  //}
   
   render() {
     return (
-        <View style={styles.container}>
-          <LabeledIcon viewContainerStyle={styles.header}
-                       iconType="ionicons"
-                       iconName="home"
-                       size={20}
-                       label="Billing Address"/>
+        <Panel>
+          <PanelHeader>
+            <LabeledIcon
+                iconType="ionicons"
+                iconName="home"
+                size={20}
+                label="Billing Address"/>
+          </PanelHeader>
           
           <Text style={styles.headerText}>
             Be sure to provide your debit or credit card's billing address.
@@ -43,53 +69,63 @@ class BillingAddress extends React.Component {
           
           <LabeledTextInput
               label="Attention (optional)"
-              viewContainerStyle={styles.inputContainer}
+              setRef={input => this.controls.attention = input}
               inputProps={{
-                textContentType: 'name'
+                textContentType: 'name',
+                blurOnSubmit: false,
+                returnKeyType: 'next',
+                onSubmitEditing: () => this.focusNextControl('addressLine1')
               }}
-              onChangeText={this.onChangeText}/>
+              onChangeText={(text) => this.onChangeText(text, 'attention')}/>
           
           <LabeledTextInput
               label="Address line 1"
-              viewContainerStyle={styles.inputContainer}
+              setRef={input => this.controls.addressLine1 = input}
               inputProps={{
-                textContentType: 'streetAddressLine1'
+                textContentType: 'streetAddressLine1',
+                blurOnSubmit: false,
+                returnKeyType: 'next',
+                onSubmitEditing: () => this.focusNextControl('addressLine2')
               }}
-              onChangeText={this.onChangeText}/>
+              onChangeText={(text) => this.onChangeText(text, 'addressLine1')}/>
           
           <LabeledTextInput
-              label="Address line 2"
-              viewContainerStyle={styles.inputContainer}
+              label="Address line 2 (required if applicable)"
+              setRef={input => this.controls.addressLine2 = input}
               inputProps={{
-                textContentType: 'streetAddressLine2'
+                textContentType: 'streetAddressLine2',
+                blurOnSubmit: false,
+                returnKeyType: 'next',
+                onSubmitEditing: () => this.focusNextControl('city')
               }}
-              onChangeText={this.onChangeText}/>
+              onChangeText={(text) => this.onChangeText(text, 'addressLine2')}/>
           
           <LabeledTextInput
               label="City"
-              viewContainerStyle={styles.inputContainer}
+              setRef={input => this.controls.city = input}
               inputProps={{
-                textContentType: 'addressCity'
+                textContentType: 'addressCity',
               }}
-              onChangeText={this.onChangeText}/>
+              onChangeText={(text) => this.onChangeText(text, 'city')}/>
           
           <LabeledPicker
               label="State"
+              setRef={input => this.controls.state = input}
               pickerStyle={{height: 40}}
-              viewContainerStyle={styles.inputContainer}
               items={[
                 {label: 'Alabama', value: 'AL'},
                 {label: 'Alaska', value: 'AK'},
                 {label: 'Arizona', value: 'AZ'}
               ]}
-              selectedValue={this.state.selectedState}
-              onValueChange={this.onStateSelected}
-          />
+              onValueChange={(value, index) => this.onChangeText(value, 'state')}/>
           
           <LabeledTextInput
               label="Zip code"
-              viewContainerStyle={styles.inputContainer}
-              onChangeText={this.onChangeText}/>
+              setRef={input => this.controls.zipCode = input}
+              inputProps={{
+                returnKeyType: 'done',
+              }}
+              onChangeText={(text) => this.onChangeText(text, 'zipCode')}/>
           
           <Button
               title="Add this address"
@@ -98,24 +134,15 @@ class BillingAddress extends React.Component {
               borderRadius={theme.btnBorderRadius}
               containerViewStyle={{marginLeft: 0, marginRight: 0}}
           />
-        </View>
+        </Panel>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
-  header: {
-    marginBottom: 20
-  },
   headerText: {
     marginBottom: 30,
     fontSize: 15
-  },
-  inputContainer: {
-    marginBottom: theme.panelInnerMargin
   }
 });
 

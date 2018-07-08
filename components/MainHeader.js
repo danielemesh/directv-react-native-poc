9e975ce8de04b48890cf0a1791f8dce98fbab113
daniel <danielemesh@gmail.com>
2018-07-08 16:38:33 +0300

    Added AT&T logo to header

--- components/MainHeader.js
+++ components/MainHeader.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Header } from 'react-native-elements';
 import theme from '../theme';
+import { Image } from 'react-native';
 
 export default MainHeader = () => (
     <Header
+        innerContainerStyles={{alignItems: 'center'}}
         backgroundColor={theme.secondaryColor}
-        centerComponent={{
-          text: 'AT&T',
-          color: '#fff',
-          style: {color: '#fff'}
-        }}
+        centerComponent={<Image resizeMode="contain" source={require('../assets/images/logo.png')} style={{height: 50}} />}
     />
 );
